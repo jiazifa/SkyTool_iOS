bc06f176039d4a3eebe46d583d4957ba72b56bd7
tree <sunshukang30@163.com>
2019-03-30 18:12:21 +0800

    render staves

--- SkyTool/Resources/vexflow/javascript/main.js
+++ SkyTool/Resources/vexflow/javascript/main.js
@@ -1,3 +1,14 @@
+var CONSTANT = {
+  elementWidth: {
+    "w": 100, 
+    "h": 80, 
+    "q": 60, 
+    "8": 50, 
+    "16": 40, 
+    "32": 40
+  }
+}
+
 var VexTranslator = function(dom) {
   var VF = Vex.Flow;
   var renderer = new VF.Renderer(dom, VF.Renderer.Backends.SVG);
@@ -8,11 +19,6 @@ var VexTranslator = function(dom) {
     dom: dom,
     notes: [],
     staves: [],
-    bars: [],
-  };
-
-  obj.resetNotes = function() {
-    app.notes = [];
   };
 
   obj.setClef = function(clef, signature) {
@@ -28,22 +34,40 @@ var VexTranslator = function(dom) {
     this.stave = stave;
   };
 
-  obj.addNote = function(clef, keys, duration) {
+  obj.createNote = function(clef, keys, duration) {
     var note = new VF.StaveNote({
       clef: clef,
       keys: keys,
       duration: duration
-    });  
-    this.notes.push(note);
-    
+    });
     return note;
   };
 
+  obj.createVoice = function(notes) {
+    var voice = new Vex.Flow.Voice({
+      num_beats: 4,
+      beat_value: 4
+    });
+    voice.addTickables(notes);
+    this.notes.push(notes);
+    this.createStave(voice);
+    return voice
+  }
+
+  obj.createStave = function(voice) {
+    var stave = new Vex.Flow.Stave(60, 40, 200);
+    stave.voice = voice;
+    voice.bar = stave;
+    this.staves.push(stave);
+  }
+
   obj.render = function() {
     var context = this.renderer.getContext();
-    console.log(this.notes);
-    
-    VF.Formatter.FormatAndDraw(context, this.stave, this.notes);
+    for (let index = 0; index < this.staves.length; index++) {
+      const stave = this.staves[index];
+      const notes = this.notes[index];
+      VF.Formatter.FormatAndDraw(context, stave, notes);
+    }
   };
 
   return obj;
