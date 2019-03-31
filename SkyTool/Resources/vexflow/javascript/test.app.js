58de8173f45103083f197ffeaa6e096a72bac382
tree <sunshukang30@163.com>
2019-03-31 20:31:12 +0800

    修改了宽度计算

--- SkyTool/Resources/vexflow/javascript/test.app.js
+++ SkyTool/Resources/vexflow/javascript/test.app.js
@@ -2,13 +2,15 @@
 window.onload = function() {
   var dom = document.getElementById("boo");
   var translator = new VexTranslator(dom);
-  translator.setClef("treble", "4/4");
   translator.createVoice([
     translator.createNote("treble", ["c/4"], "q"),
     translator.createNote("treble", ["c/4"], "q"),
     translator.createNote("treble", ["c/4"], "q"),
     translator.createNote("treble", ["c/4", "e/4", "g/4"], "q"),
   ]);
+  translator.setClef("treble", "4/4");
+  
   translator.render();
   console.log(translator.dom.clientWidth);
+  
 };
